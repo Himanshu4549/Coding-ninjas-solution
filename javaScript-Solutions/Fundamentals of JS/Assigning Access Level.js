3bed14042d73d16e248d4167e28b6a71a2e789d1
Himanshu Pandey <155448709+Himanshu4549@users.noreply.github.com>
2024-01-04 11:35:07 +0530

    Update Assigning Access Level.js

--- javaScript-Solutions/Fundamentals of JS/Assigning Access Level.js
+++ javaScript-Solutions/Fundamentals of JS/Assigning Access Level.js
@@ -1,12 +1,21 @@
-//Write the program here to get the access level of the user depending upon the jobTitle and yearsOfExperience.
-//Store your final result in the "accessLevel" variable.
-//Please do not alter anything else in the starter code
+/*
+Problem statement
+
+You are working on a system that assigns different access levels to employees based on their job titles and their years of experience.
+Write a program that assigns the access level of employees depending on their job titles and years of experience.
+
+Objectives:-
+
+1- Use the following conditions for assigning the access level:
+2- Employees with the job title Manager should be assigned Full Access regardless of the year of experience.
+3- For Employees with the job title Supervisor if the years of experience is "5 or more", then Full Access should be assigned. Otherwise, they should be assigned Partial Access.
+4- For Employees with the job title Associate, if the years of experience is 3 or more, then "Partial Access" should be assigned. Otherwise, they should be assigned "Limited Access."
+5- Employees with any other job title with 2 or more years of experience should be assigned "Limited Access." Otherwise, they should be assigned "Trainee Access."
+*/
 
 function main(jobTitle, yearsOfExperience) {
     let accessLevel;
-    //write your code here
-     
-  
+
   switch(jobTitle){
       case "Manager":
           accessLevel = "Full Access";
